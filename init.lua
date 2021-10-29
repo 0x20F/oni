bad4d95323a33a61ba375ec86d0ddfce8e58acb2
0x20F <0x20fa@gmail.com>
2021-10-29 21:49:57 +0200

    Move termguicolors to lua api

--- init.lua
+++ init.lua
@@ -37,24 +37,22 @@ require('misc.dashboard')
 require('misc.statusline')
 
 
-local options = vim.opt
+local opt = vim.opt
 
-options.smartindent = true  -- Indent based on programming language
-options.autoindent = true   -- For autoindent
-options.tabstop = 4         -- Number of visual spaces per tab
-options.shiftwidth = 4      -- Indents will have a width of 4
-options.expandtab = true    -- Tabs to spaces
-options.number = true       -- Show line numbers
-options.showmatch = true    -- Highlight matching [{()}]
-options.hlsearch = true     -- Highlight matches when searching
-options.cursorline = true   -- Highlight current line
-options.background = 'dark' -- Tell the them to use its dark colors
-options.wrap = false        -- Don't wrap really long lines
+opt.smartindent = true      -- Indent based on programming language
+opt.autoindent = true       -- For autoindent
+opt.tabstop = 4             -- Number of visual spaces per tab
+opt.shiftwidth = 4          -- Indents will have a width of 4
+opt.expandtab = true        -- Tabs to spaces
+opt.number = true           -- Show line numbers
+opt.showmatch = true        -- Highlight matching [{()}]
+opt.hlsearch = true         -- Highlight matches when searching
+opt.cursorline = true       -- Highlight current line
+opt.background = 'dark'     -- Tell the them to use its dark colors
+opt.wrap = false            -- Don't wrap really long lines
+opt.termguicolors = true    -- Make it use the full range of colors
 vim.g.colors_name = 'one'   -- Set the colorscheme
 
-
--- Things that aren't in the nvim API yet
-vim.cmd "set termguicolors"
 
 -- Normal highlight command won't work so autokey it
 vim.cmd [[ au VimEnter * highlight LineNr guibg=none ctermbg=none ]]
