02c45c0585c3acd0f44185ebcd7766a2e84dfd32
0x20F <0x20fa@gmail.com>
2021-10-29 21:41:44 +0200

    Nicer binding of multiple modes

--- lua/core/keys.lua
+++ lua/core/keys.lua
@@ -17,8 +17,18 @@ local normal = 'n'
 local visual = 'v'
 local insert = 'i'
 
-local map = function(mode, key, result)
-    vim.api.nvim_set_keymap(mode, key, result, { noremap = true, silent = true })
+-- {modes} an array of modes
+-- {key} the keyboard combo to map
+-- {result} what should happen when the combo is hit
+local map = function(modes, key, result)
+    for mode = 1, #modes do
+        vim.api.nvim_set_keymap(
+            mode,
+            key,
+            result,
+            { noremap = true, silent = true }
+        )
+    end
 end
 
 
@@ -32,18 +42,27 @@ map(normal, '<leader>a', ':NvimTreeToggle<cr>')
 
 
 -- Telescope bindings
-map(normal, '<C-p>', ':lua require"telescope.builtin".find_files()<cr>')
-map(normal, '<C-f>', ':lua require"telescope.builtin".live_grep()<cr>')
+map({ normal }, '<C-p>', ':lua require"telescope.builtin".find_files()<cr>')
+map({ normal }, '<C-f>', ':lua require"telescope.builtin".live_grep()<cr>')
+
+
 
 
 -- Split bindings
-map(normal, '<leader>j', ':split<cr>')
-map(normal, '<leader>l', ':vsplit<cr>')
+map({ normal }, '<leader>j', ':split<cr>')
+map({ normal }, '<leader>l', ':vsplit<cr>')
 
-map(insert, '<C-w>', ':q<cr>')
-map(normal, '<C-w>', ':q<cr>')
+-- Quit like in vscode, whenever
+map({ normal, insert }, '<C-w>', '<esc>:q<cr>')
 
-map(normal, '<M-k>', ':wincmd k<cr>')
-map(normal, '<M-j>', ':wincmd j<cr>')
-map(normal, '<M-h>', ':wincmd h<cr>')
-map(normal, '<M-l>', ':wincmd l<cr>')
+-- Move between split panels
+map({ normal, insert }, '<M-k>', '<esc>:wincmd k<cr>')
+map({ normal, insert }, '<M-j>', '<esc>:wincmd j<cr>')
+map({ normal, insert }, '<M-h>', '<esc>:wincmd h<cr>')
+map({ normal, insert }, '<M-l>', '<esc>:wincmd l<cr>')
+
+
+-- Save file like any other text editor
+map({ normal, insert, visual }, '<C-s>', '<esc>:w<cr>')
+
+
