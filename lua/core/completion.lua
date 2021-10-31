60017e202cae5bb9142932dfae530cc8e4a376e0
0x20F <0x20fa@gmail.com>
2021-10-31 18:15:35 +0100

    Add python server

--- lua/core/completion.lua
+++ lua/core/completion.lua
@@ -66,18 +66,26 @@ local servers = require 'nvim-lsp-installer.servers'
 
 -- Install some servers before trying to set things up
 -- Rust
-local ok, rust_analyzer = servers.get_server('rust_analyzer')
-if ok then
+local okr, rust_analyzer = servers.get_server('rust_analyzer')
+if okr then
     if not rust_analyzer:is_installed() then
         rust_analyzer:install()
     end
 end
 
 -- Lua
-local ok, sumneko_lua = servers.get_server('sumneko_lua')
-if ok then
+local oks, sumneko_lua = servers.get_server('sumneko_lua')
+if oks then
     if not sumneko_lua:is_installed() then
         sumneko_lua:install()
+    end
+end
+
+-- Python
+local okp, pylsp = servers.get_server('pylsp')
+if okp then
+    if not pylsp:is_installed() then
+        pylsp:install()
     end
 end
 
@@ -98,3 +106,4 @@ local lsp = require 'lspconfig'
 lsp.tsserver.setup { capabilities = capabilities }
 lsp.sumneko_lua.setup { capabilities = capabilities }
 lsp.rust_analyzer.setup { capabilities = capabilities }
+lsp.pylsp.setup { capabilities = capabilities }
